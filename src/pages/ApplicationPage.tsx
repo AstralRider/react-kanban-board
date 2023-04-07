6f456876e5f5f136e877078149b2287b40d64e3c
AstralRider <100898882+AstralRider@users.noreply.github.com>
2023-04-07 19:53:57 +1000

    added and styled icon to close side nav

--- src/pages/ApplicationPage.tsx
+++ src/pages/ApplicationPage.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import { authTypes } from '../types/types'
 import { IoIosSettings } from 'react-icons/io'
 import Button from '../components/Button'
-import { RiMap2Fill } from 'react-icons/ri'
 import SingleAccordion from '../components/Accordion'
+import { BsArrowLeftCircle } from 'react-icons/bs'
 
 const ApplicationPage = () => {
   const { user } = useAuth() as authTypes
@@ -26,7 +26,10 @@ const ApplicationPage = () => {
     <>
       <div className='gap flex'>
         {/* Side Nav */}
-        <div className='min-h-screen w-64 flex-none bg-gray-200'>
+        <div className='relative min-h-screen w-64 flex-none bg-gray-200'>
+          <div className=' absolute left-[245px] top-7 cursor-pointer rounded-full bg-gray-200 shadow-lg'>
+            <BsArrowLeftCircle className='text-2xl text-gray-600' />
+          </div>
           <div className='mt-5 flex flex-col gap-1'>
             <Button grey active className='mx-2.5 text-blue-500 hover:bg-gray-300'>
               <IoIosSettings /> {user?.displayName}
