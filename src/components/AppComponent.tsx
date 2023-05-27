54c68f834d5b17673c950bc49233df9d856cfa34
AstralRider <100898882+AstralRider@users.noreply.github.com>
2023-05-27 23:52:03 +1000

    separated kanban board into another component

---
+++ src/components/AppComponent.tsx
@@ -0,0 +1,162 @@
+/* eslint-disable react/prop-types */
+import { DocumentData, doc, onSnapshot } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import {
+  addCard,
+  addColumn,
+  deleteCard,
+  deleteColumn,
+  updateCardContent,
+  updateColumnName,
+  updateColumnOrder,
+  updateTaskIdsBetweenColumns,
+  updateTasksSingleColumn,
+} from '../lib/FirebaseQueries'
+
+import Column from './Column'
+import { DragDropContext } from 'react-beautiful-dnd'
+import MyModal from './Modal'
+import { StrictModeDroppable } from './Droppable'
+import { authTypes } from '../types/types'
+import { db } from '../lib/firebase'
+import { useAuth } from '../context/AuthContext'
+
+const AppComponent = () => {
+  const [boardState, setBoard] = useState<DocumentData>()
+  const { user } = useAuth() as authTypes
+  const [modalOpen, setModalOpen] = useState(false)
+
+  useEffect(() => {
+    const snapshot = onSnapshot(
+      doc(db, 'users', `${user?.uid}`, 'boards', '7s9YSlDnF5RvIv2v4mnf'),
+      (doc) => {
+        setBoard(doc.data())
+      },
+    )
+
+    return () => {
+      snapshot()
+    }
+  }, [])
+
+  function updateTasks(id: string, value: string): void {
+    updateCardContent(`${user?.uid}`, '7s9YSlDnF5RvIv2v4mnf', id, value)
+  }
+
+  function deleteTasks(cardId: string, colId: string) {
+    deleteCard(`${user?.uid}`, '7s9YSlDnF5RvIv2v4mnf', cardId, colId)
+  }
+
+  function addTasks(colId: string, content: string) {
+    addCard(`${user?.uid}`, '7s9YSlDnF5RvIv2v4mnf', content, colId)
+  }
+
+  function updateColName(colId: string, content: string) {
+    updateColumnName(`${user?.uid}`, '7s9YSlDnF5RvIv2v4mnf', colId, content)
+  }
+
+  function createColumn(colName: string) {
+    addColumn(`${user?.uid}`, '7s9YSlDnF5RvIv2v4mnf', colName)
+  }
+
+  function deleteCol(colId: string) {
+    deleteColumn(`${user?.uid}`, '7s9YSlDnF5RvIv2v4mnf', colId)
+  }
+
+  const columnList = boardState?.columnOrder.map((columnId: string, index: number) => {
+    const column = boardState.columns[columnId]
+
+    const tasks = column.taskIds?.map((taskId: string) => {
+      return boardState.cards[taskId]
+    })
+
+    return (
+      <Column
+        deleteTasks={deleteTasks}
+        updateTasks={updateTasks}
+        addTasks={addTasks}
+        updateColName={updateColName}
+        deleteCol={deleteCol}
+        key={column.id}
+        column={column}
+        cards={tasks}
+        index={index}
+      />
+    )
+  })
+
+  const onDragEnd = (result: any) => {
+    const { destination, source, draggableId, type } = result
+
+    if (!destination) return
+
+    if (destination.droppableId === source.droppableId && destination.index === source.index) return
+
+    const start = source.droppableId
+    const finish = destination.droppableId
+
+    //Check where the task comes from
+    const startColumn = boardState?.columns[source.droppableId]
+
+    //Check where it finishes
+    const finishColumn = boardState?.columns[destination.droppableId]
+
+    if (startColumn === finishColumn && type === 'task') {
+      const newList: string[] = Array.from(startColumn?.taskIds)
+      newList.splice(source.index, 1)
+      newList.splice(destination.index, 0, draggableId)
+      updateTasksSingleColumn(`${user?.uid}`, '7s9YSlDnF5RvIv2v4mnf', start, newList)
+    } else if (type === 'column') {
+      const updateColOrder: string[] = Array.from(boardState?.columnOrder)
+      updateColOrder.splice(source.index, 1)
+      updateColOrder.splice(destination.index, 0, draggableId)
+      updateColumnOrder(`${user?.uid}`, '7s9YSlDnF5RvIv2v4mnf', updateColOrder)
+    } else {
+      const startIds: string[] = Array.from(startColumn.taskIds)
+      startIds.splice(source.index, 1)
+
+      const finishIds: string[] = Array.from(finishColumn.taskIds)
+      finishIds.splice(destination.index, 0, draggableId)
+
+      updateTaskIdsBetweenColumns(
+        `${user?.uid}`,
+        '7s9YSlDnF5RvIv2v4mnf',
+        start,
+        startIds,
+        finish,
+        finishIds,
+      )
+    }
+  }
+
+  return (
+    <>
+      <MyModal isOpen={modalOpen} setIsOpen={setModalOpen} createColumn={createColumn}>
+        Add a column name
+      </MyModal>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <StrictModeDroppable droppableId='columns' direction='horizontal' type='column'>
+          {(provided) => (
+            <>
+              <div>
+                <div {...provided.droppableProps} ref={provided.innerRef} className='m-5 flex'>
+                  {columnList}
+
+                  <div className=''>{provided.placeholder} </div>
+                  <button
+                    onClick={() => setModalOpen(true)}
+                    className='flex-0 h-10 cursor-pointer rounded-md bg-gray-400/20 px-5 py-1 text-blue-600 hover:bg-gray-400/40 active:translate-y-0.5'
+                  >
+                    Add Column +
+                  </button>
+                </div>
+              </div>
+            </>
+          )}
+        </StrictModeDroppable>
+      </DragDropContext>
+    </>
+  )
+}
+
+export default AppComponent
--- src/pages/ApplicationPage.tsx
+++ src/pages/ApplicationPage.tsx
@@ -1,24 +1,11 @@
+/* eslint-disable react/prop-types */
 import { DocumentData, doc, onSnapshot } from 'firebase/firestore'
-import {
-  addBoard,
-  addCard,
-  addColumn,
-  deleteBoard,
-  deleteCard,
-  deleteColumn,
-  editBoardName,
-  getBoard,
-  updateCardContent,
-  updateColumnName,
-  updateTaskIdsBetweenColumns,
-  updateTasksSingleColumn,
-} from '../lib/firebaseQueries'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { addBoard, deleteBoard, editBoardName, getBoard } from '../lib/FirebaseQueries'
 
+import AppComponent from '../components/AppComponent'
 import { BsArrowLeftCircle } from 'react-icons/bs'
 import Button from '../components/Button'
-import Column from '../components/Column'
-import { DragDropContext } from 'react-beautiful-dnd'
 import { IoIosSettings } from 'react-icons/io'
 import SingleAccordion from '../components/Accordion'
 import { authTypes } from '../types/types'
@@ -31,25 +18,10 @@ const ApplicationPage = () => {
 
   const navigate = useNavigate()
   const [navOpen, setIsNavOpen] = useState(true)
-  const [boardState, setBoard] = useState<DocumentData>()
-
   // const subColRef = collection(db, 'users', `${user?.uid}`, 'boards')
   // let snap = getDocs(subColRef).then((snap) => {
   //   console.log(snap.docs.map((d) => d.id)[0])
   // })
-
-  useEffect(() => {
-    const snapshot = onSnapshot(
-      doc(db, 'users', `${user?.uid}`, 'boards', '7s9YSlDnF5RvIv2v4mnf'),
-      (doc) => {
-        setBoard(doc.data())
-      },
-    )
-
-    return () => {
-      snapshot()
-    }
-  }, [])
 
   //if user is not logged in, redirect to login page
   useEffect(() => {
@@ -66,83 +38,9 @@ const ApplicationPage = () => {
     { name: 'Boards', content: ['Board 1', 'Board 2', 'Board 3', 'Board 4', 'Board 5', 'Board 6'] },
   ]
 
-  function updateTasks(id: string, value: string): void {
-    updateCardContent(`${user?.uid}`, '7s9YSlDnF5RvIv2v4mnf', id, value)
-  }
-
-  function deleteTasks(cardId: string, colId: string) {
-    deleteCard(`${user?.uid}`, '7s9YSlDnF5RvIv2v4mnf', cardId, colId)
-  }
-
-  function addTasks(colId: string, content: string) {
-    addCard(`${user?.uid}`, '7s9YSlDnF5RvIv2v4mnf', content, colId)
-  }
-
-  function updateColName(colId: string, content: string) {
-    updateColumnName(`${user?.uid}`, '7s9YSlDnF5RvIv2v4mnf', colId, content)
-  }
-
-  const columnList = boardState?.columnOrder.map((columnId: string) => {
-    const column = boardState.columns[columnId]
-    const tasks = column.taskIds?.map((taskId: string) => {
-      return boardState.cards[taskId]
-    })
-
-    return (
-      <Column
-        deleteTasks={deleteTasks}
-        updateTasks={updateTasks}
-        addTasks={addTasks}
-        updateColName={updateColName}
-        key={column.id}
-        column={column}
-        cards={tasks}
-      />
-    )
-  })
-
-  const onDragEnd = (result: any) => {
-    const { destination, source, draggableId } = result
-
-    if (!destination) return
-
-    if (destination.droppableId === source.droppableId && destination.index === source.index) return
-
-    const start = source.droppableId
-    const finish = destination.droppableId
-    //Check where the task comes from
-    const startColumn = boardState?.columns[source.droppableId]
-    //Check where it finishes
-    const finishColumn = boardState?.columns[destination.droppableId]
-    if (startColumn === finishColumn) {
-      const newList: string[] = Array.from(startColumn.taskIds)
-      newList.splice(source.index, 1)
-      newList.splice(destination.index, 0, draggableId)
-
-      updateTasksSingleColumn(`${user?.uid}`, '7s9YSlDnF5RvIv2v4mnf', start, newList)
-      return
-    } else {
-      const startIds: string[] = Array.from(startColumn.taskIds)
-      startIds.splice(source.index, 1)
-
-      const finishIds: string[] = Array.from(finishColumn.taskIds)
-      finishIds.splice(destination.index, 0, draggableId)
-
-      updateTaskIdsBetweenColumns(
-        `${user?.uid}`,
-        '7s9YSlDnF5RvIv2v4mnf',
-        start,
-        startIds,
-        finish,
-        finishIds,
-      )
-    }
-  }
-
   return (
     <>
       <div className='flex'>
-        {/* Side Nav */}
         <div
           className={`duration-500 ${
             navOpen ? 'relative min-h-screen w-64 flex-none bg-gray-200' : 'w-0 grow-0 ease-out'
@@ -170,12 +68,7 @@ const ApplicationPage = () => {
 
         <div className={`flex min-h-screen grow bg-gray-300`}>
           <div className='mx-10 my-5 h-screen w-full rounded-md '>
-            <DragDropContext onDragEnd={onDragEnd}>
-              <div className='m-5 flex gap-4'>
-                {columnList}
-                <div>Add Col</div>
-              </div>
-            </DragDropContext>
+            <AppComponent />
           </div>
         </div>
       </div>
