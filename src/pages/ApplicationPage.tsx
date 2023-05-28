75d532eecc80a857da7768dcc985d0b6ad5423e6
AstralRider <100898882+AstralRider@users.noreply.github.com>
2023-05-28 22:56:43 +1000

    updated application page

--- src/pages/ApplicationPage.tsx
+++ src/pages/ApplicationPage.tsx
@@ -7,6 +7,7 @@ import AppComponent from '../components/AppComponent'
 import { BsArrowLeftCircle } from 'react-icons/bs'
 import Button from '../components/Button'
 import { IoIosSettings } from 'react-icons/io'
+import NavBar from '../components/NavBar'
 import SingleAccordion from '../components/Accordion'
 import { authTypes } from '../types/types'
 import { db } from '../lib/firebase'
@@ -40,38 +41,13 @@ const ApplicationPage = () => {
 
   return (
     <>
-      <div className='flex'>
-        <div
-          className={`duration-500 ${
-            navOpen ? 'relative min-h-screen w-64 flex-none bg-gray-200' : 'w-0 grow-0 ease-out'
-          }`}
-        >
-          <BsArrowLeftCircle
-            onClick={handleNavClick}
-            className={`${
-              navOpen
-                ? 'absolute top-12 left-[245px]'
-                : 'absolute top-12 left-[-5px] rotate-180 overflow-hidden'
-            } absolute z-10 cursor-pointer select-none rounded-full bg-gray-200 text-2xl text-gray-600 shadow-lg duration-500  hover:scale-125`}
-          />
-
-          <div className='mt-5 flex flex-col gap-1'>
-            <Button active className='mx-2.5 text-blue-500 hover:bg-gray-300'>
-              <IoIosSettings /> {user?.displayName}
-            </Button>
-            <SingleAccordion items={items} />
-          </div>
-        </div>
-        {/* End Side Nav */}
-
-        {/* Main App Section */}
-
-        <div className={`flex min-h-screen grow bg-gray-300`}>
-          <div className='mx-10 my-5 h-screen w-full rounded-md '>
-            <AppComponent />
-          </div>
+      {/* Main App Section */}
+      <div className={`flex min-h-screen grow bg-gray-300`}>
+        <div className='mx-10 my-5 h-screen w-full rounded-md '>
+          <AppComponent />
         </div>
       </div>
+
       {/* End main container */}
     </>
   )
