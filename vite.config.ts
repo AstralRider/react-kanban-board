fe47e32a8048d3973db611a77eb1c9b20c1bcfb8
AstralRider <100898882+AstralRider@users.noreply.github.com>
2023-06-18 00:01:20 +1000

    config adjustment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    },
+  }
 })
